7b71010fe496ed993fe0a0d8e063d58bcccc9304
Malte Wrogemann <wroge.malte@gmail.com>
2024-06-29 12:44:13 +0200

    init

---
+++ README.md
@@ -0,0 +1,88 @@
+# sqlt - SQL Templates
+
+This module uses (or abuses) Go's template engine to create a SQL builder and ORM.  
+Just take a look at it and let me know what you think of this approach.  
+Might be dumb, but it surprisingly works pretty well.  
+
+```go
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/Masterminds/sprig/v3"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/wroge/sqlt"
+)
+
+type Book struct {
+	ID        int64
+	Title     string
+	CreatedAt time.Time
+}
+
+func main() {
+	ctx := context.Background()
+
+	db, err := sql.Open("sqlite3", "file:test.db?cache=shared&mode=memory")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, created_at DATE)")
+	if err != nil {
+		panic(err)
+	}
+
+	t := sqlt.New("db", "?", false).Funcs(sprig.TxtFuncMap())
+
+	insert := t.New("insert").MustParse(`
+		INSERT INTO books (title, created_at) VALUES
+		{{ range $i, $t := . }} {{ if $i }}, {{ end }}
+			({{ $t }}, {{ now }})
+		{{ end }}
+		RETURNING id;
+		{{ Int64 Dest }}
+	`)
+
+	ids, err := sqlt.QueryAll[int64](ctx, db, insert, []string{
+		"The Bitcoin Standard",
+		"Sapiens: A Brief History of Humankind",
+		"100 Go Mistakes and How to Avoid Them",
+		"Mastering Bitcoin",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(ids)
+	// [1 2 3, 4]
+
+	query := t.New("query").MustParse(`
+		SELECT 
+			id, 		{{ Int64 Dest.ID }}
+			title, 		{{ String Dest.Title }}
+			created_at 	{{ Time Dest.CreatedAt }}
+		FROM books 
+		WHERE instr(title, {{ .Search }}) > 0
+	`)
+
+	books, err := sqlt.QueryAll[Book](ctx, db, query, map[string]any{
+		"Search": "Bitcoin",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(books)
+	// [{1 The Bitcoin Standard 2024-06-29 12:32:35.41204 +0200 +0200} {4 Mastering Bitcoin 2024-06-29 12:32:35.412049 +0200 +0200}]
+}
+```
+
+The inspiration comes from:
+
+- [VauntDev/tqla](https://github.com/VauntDev/tqla)
+- [mhilton/sqltemplate](https://github.com/mhilton/sqltemplate)
---
+++ sqlt.go
@@ -0,0 +1,450 @@
+package sqlt
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"text/template"
+	"text/template/parse"
+	"time"
+)
+
+type DB interface {
+	QueryContext(ctx context.Context, sql string, args ...any) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, sql string, args ...any) *sql.Row
+	ExecContext(ctx context.Context, sql string, args ...any) (sql.Result, error)
+}
+
+func InTx(ctx context.Context, opts *sql.TxOptions, db *sql.DB, do func(db DB) error) error {
+	var (
+		tx  *sql.Tx
+		err error
+	)
+
+	tx, err = db.BeginTx(ctx, opts)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	return do(tx)
+}
+
+func Exec(ctx context.Context, db DB, t *Template, params any) (sql.Result, error) {
+	sql, args, err := t.ToSQL(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return db.ExecContext(ctx, sql, args...)
+}
+
+func Query(ctx context.Context, db DB, t *Template, params any) (*sql.Rows, error) {
+	sql, args, err := t.ToSQL(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return db.QueryContext(ctx, sql, args...)
+}
+
+func QueryRow(ctx context.Context, db DB, t *Template, params any) (*sql.Row, error) {
+	sql, args, err := t.ToSQL(params)
+	if err != nil {
+		return nil, err
+	}
+
+	return db.QueryRowContext(ctx, sql, args...), nil
+}
+
+func QueryAll[Dest any](ctx context.Context, db DB, t *Template, params any) ([]Dest, error) {
+	var (
+		dest   []any
+		mapper []func() error
+		value  = new(Dest)
+		list   []Dest
+	)
+
+	sql, args, err := t.Funcs(template.FuncMap{
+		"Dest": func() any {
+			return value
+		},
+	}).ToSQL(params, func(arg any) (string, bool) {
+		switch a := arg.(type) {
+		case Scanner:
+			dest = append(dest, a.Dest)
+			mapper = append(mapper, a.Map)
+
+			return "", true
+		}
+
+		return "", false
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := db.QueryContext(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		if err = rows.Scan(dest...); err != nil {
+			return nil, err
+		}
+
+		for _, m := range mapper {
+			if m == nil {
+				continue
+			}
+
+			if err = m(); err != nil {
+				return nil, err
+			}
+		}
+
+		list = append(list, *value)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	if err = rows.Close(); err != nil {
+		return nil, err
+	}
+
+	return list, nil
+}
+
+func QueryFirst[Dest any](ctx context.Context, db DB, t *Template, params any) (Dest, error) {
+	var (
+		dest   []any
+		mapper []func() error
+		value  = new(Dest)
+	)
+
+	sql, args, err := t.Funcs(template.FuncMap{
+		"Dest": func() any {
+			return value
+		},
+	}).ToSQL(params, func(arg any) (string, bool) {
+		switch a := arg.(type) {
+		case Scanner:
+			dest = append(dest, a.Dest)
+			mapper = append(mapper, a.Map)
+
+			return "", true
+		}
+
+		return "", false
+	})
+	if err != nil {
+		return *value, err
+	}
+
+	row := db.QueryRowContext(ctx, sql, args...)
+
+	if err = row.Scan(dest...); err != nil {
+		return *value, err
+	}
+
+	for _, m := range mapper {
+		if m == nil {
+			continue
+		}
+
+		if err = m(); err != nil {
+			return *value, err
+		}
+	}
+
+	return *value, nil
+}
+
+type Scanner struct {
+	Dest any
+	Map  func() error
+}
+
+type Raw string
+
+func New(name string, placeholder string, positional bool) *Template {
+	return &Template{
+		text: template.New(name).Funcs(template.FuncMap{
+			"Raw": func(sql string) Raw {
+				return Raw(sql)
+			},
+			"Dest": func() any {
+				return nil
+			},
+			"Scanner": func(dest sql.Scanner) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"ByteSlice": func(dest *[]byte) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"String": func(dest *string) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"Int32": func(dest *int32) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"Int64": func(dest *int64) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"Bool": func(dest *bool) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"Time": func(dest *time.Time) Scanner {
+				return Scanner{
+					Dest: dest,
+				}
+			},
+			"ParseTime": func(layout string, dest *time.Time) Scanner {
+				var data string
+
+				return Scanner{
+					Dest: &data,
+					Map: func() error {
+						v, err := time.Parse(layout, data)
+						if err != nil {
+							return err
+						}
+
+						*dest = v
+
+						return nil
+					},
+				}
+			},
+			"JsonRaw": func(dest *json.RawMessage) Scanner {
+				var data []byte
+
+				return Scanner{
+					Dest: &data,
+					Map: func() error {
+						return json.Unmarshal(data, dest)
+					},
+				}
+			},
+			"JsonMap": func(dest *map[string]any) Scanner {
+				var data []byte
+
+				return Scanner{
+					Dest: &data,
+					Map: func() error {
+						var m map[string]any
+
+						if err := json.Unmarshal(data, &m); err != nil {
+							return err
+						}
+
+						*dest = m
+
+						return nil
+					},
+				}
+			},
+			"SplitString": func(sep string, dest *[]string) Scanner {
+				var data string
+
+				return Scanner{
+					Dest: &data,
+					Map: func() error {
+						*dest = strings.Split(sep, data)
+
+						return nil
+					},
+				}
+			},
+		}),
+		placeholder: placeholder,
+		positional:  positional,
+	}
+}
+
+type Template struct {
+	text        *template.Template
+	placeholder string
+	positional  bool
+}
+
+func (t *Template) New(name string) *Template {
+	return &Template{
+		text:        t.text.New(name),
+		placeholder: t.placeholder,
+		positional:  t.positional,
+	}
+}
+
+func (t *Template) Option(opt ...string) *Template {
+	t.text.Option(opt...)
+
+	return t
+}
+
+func (t *Template) Parse(sql string) (*Template, error) {
+	text, err := t.text.Parse(sql)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Template{
+		text:        escape(text),
+		placeholder: t.placeholder,
+		positional:  t.positional,
+	}, nil
+}
+
+func (t *Template) MustParse(sql string) *Template {
+	n, err := t.Parse(sql)
+	if err != nil {
+		panic(err)
+	}
+
+	return n
+}
+
+func (t *Template) Funcs(fm template.FuncMap) *Template {
+	t.text.Funcs(fm)
+
+	return t
+}
+
+func (t *Template) Lookup(name string) *Template {
+	text := t.text.Lookup(name)
+	if text == nil {
+		return nil
+	}
+
+	return &Template{
+		text:        text,
+		placeholder: t.placeholder,
+		positional:  t.positional,
+	}
+}
+
+func (t *Template) ToSQL(params any, inject ...func(arg any) (string, bool)) (string, []any, error) {
+	var (
+		buf  strings.Builder
+		args []any
+	)
+
+	t.text.Funcs(template.FuncMap{
+		ident: func(arg any) string {
+			for _, inj := range inject {
+				if sql, ok := inj(arg); ok {
+					return sql
+				}
+			}
+
+			switch a := arg.(type) {
+			case Raw:
+				return string(a)
+			case Scanner:
+				return ""
+			}
+
+			args = append(args, arg)
+
+			if t.positional {
+				return fmt.Sprintf("%s%d", t.placeholder, len(args))
+			}
+
+			return t.placeholder
+		},
+	})
+
+	if err := t.text.Execute(&buf, params); err != nil {
+		return "", nil, err
+	}
+
+	return buf.String(), args, nil
+}
+
+var ident = "__sqlt__"
+
+// stolen from here: https://github.com/mhilton/sqltemplate/blob/main/escape.go
+func escape(text *template.Template) *template.Template {
+	for _, tpl := range text.Templates() {
+		escapeTree(tpl.Tree)
+	}
+
+	return text
+}
+
+func escapeTree(s *parse.Tree) *parse.Tree {
+	if s.Root == nil {
+		return s
+	}
+
+	escapeNode(s, s.Root)
+
+	return s
+}
+
+func escapeNode(s *parse.Tree, n parse.Node) {
+	switch v := n.(type) {
+	case *parse.ActionNode:
+		escapeNode(s, v.Pipe)
+	case *parse.IfNode:
+		escapeNode(s, v.List)
+		escapeNode(s, v.ElseList)
+	case *parse.ListNode:
+		if v == nil {
+			return
+		}
+		for _, n := range v.Nodes {
+			escapeNode(s, n)
+		}
+	case *parse.PipeNode:
+		if len(v.Decl) > 0 {
+			return
+		}
+
+		if len(v.Cmds) < 1 {
+			return
+		}
+
+		cmd := v.Cmds[len(v.Cmds)-1]
+		if len(cmd.Args) == 1 && cmd.Args[0].Type() == parse.NodeIdentifier && cmd.Args[0].(*parse.IdentifierNode).Ident == ident {
+			return
+		}
+
+		v.Cmds = append(v.Cmds, &parse.CommandNode{
+			NodeType: parse.NodeCommand,
+			Args:     []parse.Node{parse.NewIdentifier(ident).SetTree(s).SetPos(cmd.Pos)},
+		})
+	case *parse.RangeNode:
+		escapeNode(s, v.List)
+		escapeNode(s, v.ElseList)
+	case *parse.WithNode:
+		escapeNode(s, v.List)
+		escapeNode(s, v.ElseList)
+	}
+}
